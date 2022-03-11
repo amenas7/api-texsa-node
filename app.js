fffdb0ac1f4a61440206771f2815c19c30a4f52c
Alvaro Mena <amenas94@gmail.com>
2022-03-11 13:13:41 -0500

    demo dirname

--- app.js
+++ app.js
@@ -32,10 +32,10 @@ app.use( cors({ origin: true, credentials: true }) );
 
 // body parser
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.json( { limit: '50mb' } ))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(fileUpload({
 //     useTempFiles : true,
