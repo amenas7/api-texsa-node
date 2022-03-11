1d919a7068ddf529290b296a5118147e0c5a140e
Alvaro Mena <amenas94@gmail.com>
2022-03-11 12:39:20 -0500

    demo

--- app.js
+++ app.js
@@ -32,8 +32,10 @@ app.use( cors({ origin: true, credentials: true }) );
 
 // body parser
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 app.use(bodyParser.json( { limit: '50mb' } ))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
+app.use(express.json());
+
 
 // app.use(fileUpload({
 //     useTempFiles : true,
