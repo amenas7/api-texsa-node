7dd6ffbe2539a7b8174b973af4fd42631befd766
Alvaro Mena <amenas94@gmail.com>
2022-03-11 16:56:32 -0500

    demo

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use( cors({ origin: true, credentials: true }) );
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public'))); 
 
 // app.use(fileUpload({
 //     useTempFiles : true,
