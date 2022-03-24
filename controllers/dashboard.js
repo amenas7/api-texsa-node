77763505833fd293f19a60bdc0d77e2c2a8b6b97
Alvaro Mena <amenas94@gmail.com>
2022-03-23 21:58:24 -0500

    demo

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -87,7 +87,7 @@ function consultar_tabla_uno(req, res, p_desde, p_hasta) {
     AND "${p_hasta}") as total_pen_todas, 
     (select format(IFNULL(sum(total_g_peso),0),0,'de_DE') total_g_peso from coti where tipo_moneda = 'Peso' AND date_format(fecha_reg, "%Y-%m-%d") BETWEEN "${p_desde}" 
     AND "${p_hasta}") as total_peso_todas, 
-    (select format(IFNULL(sum(total_g_dolar),0),0),2,'en_US') total_g_dolar from coti where tipo_moneda = 'Dolar' AND date_format(fecha_reg, "%Y-%m-%d") BETWEEN "${p_desde}" 
+    (select format(IFNULL(sum(total_g_dolar),0),2,'en_US') total_g_dolar from coti where tipo_moneda = 'Dolar' AND date_format(fecha_reg, "%Y-%m-%d") BETWEEN "${p_desde}" 
     AND "${p_hasta}") as total_dolar_todas`;
 
     //return console.log(query);
